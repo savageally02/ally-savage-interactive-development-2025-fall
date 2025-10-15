4cd9b4432cbb22708d3a584983532aa058c6cfb6
Ally <savageally02@gmail.com>
2025-10-15 14:16:30 -0500

    yerp

---
+++ 2025-10-09/javascript/script.js
@@ -0,0 +1,17 @@
+// $("p").on("click", function(){
+//     if($("p").hasClass("colored-text")){
+//        $("p").removeClass("colored-text");
+//     }else{
+//         $("p").addClass("colored-text");
+//     }
+
+// })
+
+
+// $("p").on("click", function(){
+//     $("p").toggleClass("colored-text");
+// })
+
+$("p").on("click", function(){
+    $(this).toggleClass("colored-text");
+})
